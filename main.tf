babed469e2d14399ad779ea4f78056c776b10d79
Nishant <37601608+00adam001@users.noreply.github.com>
2024-03-04 15:41:14 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-app-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-app-service"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    python_version = "3.8"
+  }
+
+  app_settings = {
+    "FLASK_APP" = "app.py"
+    "WEBSITE_RUN_FROM_PACKAGE" = "https://github.com/00adam001/assignment_3/archive/refs/heads/main.zip"
+  }
+}
