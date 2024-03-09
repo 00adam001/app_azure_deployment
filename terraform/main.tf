f573062399ee7d2a4b4994f57468970ad7e76425
Nishant <37601608+00adam001@users.noreply.github.com>
2024-03-09 00:38:49 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.46.0"
-    }
+  backend "azurerm" {
+    resource_group_name  = "TerraformHW3"
+    storage_account_name = "terraformhw3storage"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
   }
 }
 
