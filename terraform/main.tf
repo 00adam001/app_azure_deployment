03cc9f0813b3c5f93a40aa7f200e27793bab5f46
Nishant <37601608+00adam001@users.noreply.github.com>
2024-03-09 00:26:15 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,45 +2,28 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "2.46.0"
     }
   }
-
-   backend "azurerm" {
-    resource_group_name  = "friday-demo-rg"
-    storage_account_name = "sttfstatemgt01"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
-  }
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
+provider "azurerm" {
+  features {}
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-app-service-plan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
+resource "azurerm_resource_group" "arg" {
+  name     = "TerraformHW3"
+  location = "West Europe"
 }
 
-resource "azurerm_app_service" "example" {
-  name                = "example-app-service"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+resource "azurerm_storage_account" "asa" {
+  name                     = "terraformhw3storage"
+  resource_group_name      = "TerraformHW3"
+  account_tier             = "Standard"
+  location                 = "West Europe"
+  account_replication_type = "LRS"
 
-  site_config {
-    python_version = "3.8"
-  }
-
-  app_settings = {
-    "FLASK_APP" = "app.py"
-    "WEBSITE_RUN_FROM_PACKAGE" = "https://github.com/00adam001/assignment_3/archive/refs/heads/main.zip"
+  tags = {
+    environment = "production"
   }
 }
