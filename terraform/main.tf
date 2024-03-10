3d52fbfeb131882bb0ac9be10de5ee9e08e5f01f
Nishant <37601608+00adam001@users.noreply.github.com>
2024-03-10 18:03:24 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,13 @@ resource "azurerm_storage_account" "asa" {
     environment = "production"
   }
 }
+
+  site_config {
+    python_version = "3.8"
+  }
+
+  app_settings = {
+    "FLASK_APP" = "app.py"
+    "WEBSITE_RUN_FROM_PACKAGE" = "https://github.com/00adam001/assignment_3/archive/refs/heads/main.zip"
+  }
+}
